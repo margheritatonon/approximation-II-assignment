c93142bf4b1c7b9bbc033692b2aaf9ad18ec13a7
Margherita <mtonon.ieu2023@student.ie.edu>
2025-04-12 13:24:31 +0200

    Created a script to plot a figure used in the written assignment

---
+++ sampling_visuals.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sampling_aliasing import generate_signal
+
+frequency = 2
+sampling_frequency = 5
+t_continuous, x_continuous = generate_signal(2, frequency, plot = True)
+
+def sample_signal(x, t, fs, plot_one = False):
+    """
+    Samples the continuous time signal x at a rate fs.
+    t: the time interval of the sampled signal x
+    Returns the time and amplitude arrays of the sampled signal
+    If plot == True, plots the continuous signal along with the sampled points.
+    """
+    Ts = 1/fs #defining the sampling period
+    t_sampled = np.arange(t[0], t[-1], Ts) #the sampled time points
+    x_sampled = np.interp(t_sampled, t, x) #using np.interp to sample
+
+    if plot_one == True:
+        plt.plot(t, x, label = "Original")
+        #plt.scatter(t_sampled, x_sampled, label = "Sampled", color = "red")
+        plt.stem(t_sampled, x_sampled, linefmt='r', markerfmt='ro', basefmt='None', label = "Sampled")
+        plt.title(f"Original Signal versus Sampled Signal (Sampling Frequency {fs} Hertz)")
+        plt.xlabel("Time (s)")
+        plt.ylabel("Amplitude")
+        plt.grid(True)
+        plt.legend()
+        plt.show()
+    
+    return t_sampled, x_sampled
+
+sample_signal(x_continuous, t_continuous, sampling_frequency, plot_one = True)
